06727d1a9044ebbad49c6d007db6bb2d4b575954
Exiquio Cooper-Anderson <exiquio@gmail.com>
2026-07-04 15:27:26 +0700

    Modularized config

--- init.lua
+++ init.lua
@@ -1,148 +1,27 @@
--- [[ LEADER KEY ]]
+-- [[ GLOBALS ]]
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- Leader key
+-- WARNING: Required before loading plugins
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Nerd fonts (must be configured/installed in terminal)
+-- Nerd fonts - Must be installed on host
 vim.g.have_nerd_font = true
 
--- [[ OPTIONS ]]
--- See `:help vim.opt`
--- For more options, you can see `:help option-list`
+-- [[ CONFIGURATIONS ]]
 
--- Indentation settings
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
--- Make line numbers default
-vim.opt.number = true
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
--- Sync clipboard between OS and Neovim.
--- Schedule the setting after `UiEnter` because it can increase startup-time.
--- Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
--- Enable break indent
-vim.opt.breakindent = true
--- Save undo history
-vim.opt.undofile = true
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
---vim.opt.ignorecase = true
---vim.opt.smartcase = true
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
--- Decrease update time
-vim.opt.updatetime = 250
--- Decrease mapped sequence wait time
--- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
--- Show which line your cursor is on
-vim.opt.cursorline = true
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- [[ KEYMAPS ]]
--- See `:help vim.keymap.set()`
-
--- Clear highlights on search when pressing <Esc> in normal mode
--- See `:help hlsearch`
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
--- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
--- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
--- CodeCompanion Keymaps (Kickstart Style)
-vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", { desc = "[A]I [C]hat Toggle" })
-vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CodeCompanionActions<cr>", { desc = "[A]I [A]ctions Menu" })
-vim.keymap.set({ "n", "v" }, "<leader>ai", "<cmd>CodeCompanion<cr>", { desc = "[A]I [I]nline Prompt" })
-vim.keymap.set("v", "<leader>as", "<cmd>CodeCompanionChat Add<cr>", { desc = "[A]I [S]election to Chat" })
-
--- [[ AUTOCOMMANDS ]]
--- See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
--- Try it with `yap` in normal mode
--- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
--- [[ `lazy.nvim` PLUGIN MANAGER ]]
--- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-
--- Install `lazy.nvim`
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+require("config.options")
+require("config.keymaps")
+require("config.autocommands")
 
 -- [[ PLUGIN INSTALLATION]]
--- To check the current status of your plugins, run :Lazy
--- You can press `?` in this menu for help. Use `:q` to close the window
--- To update plugins you can run :Lazy update
-
--- Install plugins
--- NOTE:
--- * Plugins can be added with a link (or for a github repo: 'owner/repo' link).
--- * Plugins can also be added by using a table, with the first argument being the link and the following
---   keys can be used to configure plugin behavior/loading/etc.
--- * Plugins can also be configured to run Lua code when they are loaded.
--- * Plugins can specify dependencies.
---   * The dependencies are proper plugin specifications as well - anything you do for a plugin at the top level,
---     you can do for a dependency.
--- * Use `opts = {}` to force a plugin to be loaded.
+-- Using Lazy.nvim
+-- Run `:Lazy` for plugin status
 
 require("lazy").setup({
 	{ import = "custom.plugins" },
 }, {
 	ui = {
-		-- If you are using a Nerd Font: set icons to an empty table which will use the
-		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
 		icons = vim.g.have_nerd_font and {} or {
 			cmd = "⌘",
 			config = "🛠",
@@ -162,5 +41,6 @@ require("lazy").setup({
 })
 
 -- [[ MISC. ]]
+
 -- Override the default colorcolumn to a subtle Dracula Pro dark purple-gray
 vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#5c4ade" })
