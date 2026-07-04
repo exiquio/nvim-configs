93e727e7a1ff5f571c72322c33b92fddea9dc251
Exiquio Cooper-Anderson <exiquio@gmail.com>
2026-07-04 13:33:35 +0700

    Removed vim-sleuth - it's annoying

--- init.lua
+++ init.lua
@@ -171,8 +171,6 @@ require("lazy").setup({
 			})
 		end,
 	},
-	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
 
 	-- Adds git related signs to the gutter, as well as utilities for managing changes.
 	-- See `:help gitsigns` to understand what the configuration keys do
