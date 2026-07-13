20a39c64f09cd658c517370c73090a2203bbb459
Exiquio Cooper-Anderson <exiquio@gmail.com>
2026-07-13 21:58:56 +0700

    Audit comments added to todo-comments config

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -10,6 +10,17 @@ M.plugins = {
 M.config = function()
 	require("todo-comments").setup({
 		signs = false,
+		keywords = {
+			AUDIT = { icon = " ", color = "info" },
+			AUDIT_INFO = { icon = " ", color = "hint" },
+			AUDIT_OK = { icon = " ", color = "test" },
+			AUDIT_WARN = { icon = " ", color = "warning" },
+			AUDIT_FAIL = { icon = " ", color = "error" },
+			AUDIT_FIX = { icon = " ", color = "error" },
+			AUDIT_TODO = { icon = " ", color = "info" },
+			AUDIT_NOTE = { icon = " ", color = "hint" },
+			AUDIT_QUESTION = { icon = " ", color = "hint" },
+		},
 	})
 end
 
