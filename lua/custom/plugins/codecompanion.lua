0be1d277cdafe68e8e7684e6761c350a322fe5d6
Exiquio Cooper-Anderson <exiquio@gmail.com>
2026-07-22 13:25:57 +0700

    Modified codecompnaion config to get it prepped for working with both host and guess vm.

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -9,27 +9,29 @@ M.plugins = {
 M.config = function()
   require("codecompanion").setup({
     adapters = {
-      deepseek = function()
-        return require("codecompanion.adapters").extend("deepseek", {
-          env = {
-            api_key = "DEEPSEEK_API_KEY",
-          },
-          schema = {
-            model = {
-              default = "deepseek-v4-flash",
+      http = {
+        deepseek = function()
+          return require("codecompanion.adapters").extend("deepseek", {
+            env = {
+              api_key = "DEEPSEEK_API_KEY",
             },
-            temperature = {
-              default = 0,
+            schema = {
+              model = {
+                default = "deepseek-v4-flash",
+              },
+              temperature = {
+                default = 0,
+              },
+              max_tokens = {
+                default = 8192,
+              },
+              top_p = {
+                default = 1,
+              },
             },
-            max_tokens = {
-              default = 8192,
-            },
-            top_p = {
-              default = 1,
-            },
-          },
-        })
-      end,
+          })
+        end,
+      },
     },
     strategies = {
       chat = { adapter = { name = "deepseek", model = "deepseek-v4-pro" } },
